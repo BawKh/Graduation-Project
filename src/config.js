d29ab0627eeb31212d49646b3cf6a2e05f7cfaf9
BawKh <baselwael2222@gmail.com>
2024-07-03 06:41:17 +0300

    The Last Updates

---
+++ src/config.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+const connect = mongoose.connect("mongodb://localhost:27017/osamadb");
+
+// Check database connected or not
+connect
+    .then(() => {
+        console.log("Database Connected Successfully");
+    })
+    .catch(() => {
+        console.log("Database cannot be Connected");
+    });
+
+// Create Schema
+const Loginschema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    fullname: {
+        type: String,
+    },
+    email: {
+        type: String,
+    },
+    age: {
+        type: String,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+});
+
+// collection part
+const collection = new mongoose.model("users", Loginschema);
+
+module.exports = collection;
